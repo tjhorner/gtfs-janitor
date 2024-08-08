ec98eed5349cb3347fd81c7ea79de19be3475ad9
TJ Horner <me@tjhorner.com>
2024-08-08 16:52:20 -0700

    add trolleybus to overpass query

--- src/lib/osm/query.ts
+++ src/lib/osm/query.ts
@@ -8,7 +8,7 @@ export async function getStopElementsInBbox(bbox: string) {
       node[~"^(.+:)?highway$"~"^bus_stop$"];
       node[~"^(.+:)?railway$"~"^tram_stop$"];
       node[~"^(.+:)?amenity$"~"^ferry_terminal$"];
-      node[~"bus|tram|ferry"~"^yes$"];
+      node[~"bus|tram|ferry|trolleybus"~"^yes$"];
     )->.ptStops;
 
     way(bn.ptStops)["highway"~"motorway|trunk|primary|secondary|tertiary|unclassified|residential|service"]->.roadWaysWithStops;
